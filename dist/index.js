22e3245cee0f2d8a641578fe980238c9302cf4e3
Bruno <bruno.rp45@gmail.com>
2021-09-09 17:26:10 -0300

    instalando nodemon

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Matematica_1 = __importDefault(require("./Matematica"));
-let n1 = 12;
+let n1 = 15;
 let n2 = 2;
 console.log(`Soma ${n1}+${n2} = ${Matematica_1.default.subtrair(n1, n2)} `);
