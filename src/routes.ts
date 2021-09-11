ffec32a3badb96730b301fedbd9edb5c2e603ea7
Bruno <bruno.rp45@gmail.com>
2021-09-11 20:40:31 -0300

    instalando mongoose, cors, dotenv

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,8 @@
 import express from 'express';
+
 const Email  = require('./Controllers/email');
-
-
 const routes = express.Router();
 
 routes.get('/', Email.index);
 
-module.exports = routes
+module.exports = routes;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,34 @@
 import express, { Request, Response } from 'express';
+const mongoose =  require ('mongoose');
+const cors = require('cors');
+const routes = require ('./routes');
+const dotenv = require ('dotenv');
 
+dotenv.config();
 
-const routes = require('./routes');
 
 const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+const uri = process.env.URI
+
+
+mongoose.connect(`${uri}`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}, function (err: Error) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('MongoDB CONECTADO com sucesso!');     
+    };
+});
 
 
 app.use(routes);
+
 app.listen(3000, ()=> {
-    console.log('Servidor Rodando na porta 3000');
+    console.log(`Server runing on port ${port}`);
     
 });
